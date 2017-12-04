830d593f87caa7fd30b9f75a45517c2fe5adbe5d
Mohammed Hajajate <mohammedh@Firsts-iMac.local>
2017-12-04 13:46:55 +0000

    Initial Commit

---
+++ FbAlbumApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FbAlbumApp
+//
+//  Created by Mohammed Hajajate on 12/4/17.
+//  Copyright © 2017 Mohammed Hajajate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
