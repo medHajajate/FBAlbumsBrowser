67d095d56bcdab09938be3ad669cedb5b96cee65
Mohammed Hajajate <mohammedh@Firsts-iMac.local>
2017-12-07 18:12:58 +0000

    create albumsView

---
+++ FbAlbumApp/AlbumsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AlbumsViewController.swift
+//  FbAlbumApp
+//
+//  Created by Mohammed Hajajate on 12/7/17.
+//  Copyright © 2017 Mohammed Hajajate. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "my Albums"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
