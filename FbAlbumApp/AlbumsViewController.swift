99f9260ba07f9bf1f27b25a334a37bf365edee2c
Mohammed Hajajate <mohammedh@Firsts-iMac.local>
2017-12-07 18:27:16 +0000

    add tableview to albumsView

--- FbAlbumApp/AlbumsViewController.swift
+++ FbAlbumApp/AlbumsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class AlbumsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "my Albums"
@@ -22,4 +23,16 @@ class AlbumsViewController: UIViewController {
     }
     
 
+}
+
+extension AlbumsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "albumCell", for: indexPath)
+        return cell
+    }
 }
