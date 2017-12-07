a66a7f13aadcf62cbe368e1578c4c529707ff947
Mohammed Hajajate <mohammedh@Firsts-iMac.local>
2017-12-07 18:05:03 +0000

    Log in to facebook

--- FbAlbumApp/ViewController.swift
+++ FbAlbumApp/ViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
+    @IBOutlet weak var loginButton: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loginButton.readPermissions = ["email", "user_photos"];
+        loginButton.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error == nil {
+                Log.msg(message: "Log In")
+        } else {
+            if let error = error?.localizedDescription {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {}
 
 
 }
